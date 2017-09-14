7c851a7e946710fd0c8e56e901fa56a43bf61bc9
Yaniv Efraim <yaniv.efraim@gmail.com>
2017-09-14 14:22:56 +0300

    refactor to drivers

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -3,18 +3,22 @@ import {expect} from 'chai';
 import {mount} from 'enzyme';
 import App from './App';
 
+let wrapper;
+const render = () => mount(
+  <App/>, {attachTo: document.createElement('div')}
+);
+const clickCellAt = index => wrapper.find('[data-hook="cell"]').at(index).simulate('click');
+const getCellTextAt = index => wrapper.find('[data-hook="cell"]').at(index).text();
+
 describe('App', () => {
-  let wrapper;
 
   afterEach(() => wrapper.detach());
 
   it('should have "O" after second user clicks', () => {
-    wrapper = mount(
-      <App/>, {attachTo: document.createElement('div')}
-    );
-    wrapper.find('[data-hook="cell"]').at(0).simulate('click');
-    expect(wrapper.find('[data-hook="cell"]').at(0).text()).to.eq('X');
-    wrapper.find('[data-hook="cell"]').at(1).simulate('click');
-    expect(wrapper.find('[data-hook="cell"]').at(1).text()).to.eq('O');
+    wrapper = render();
+    clickCellAt(0);
+    expect(getCellTextAt(0)).to.eq('X');
+    clickCellAt(1);
+    expect(getCellTextAt(1)).to.eq('O');
   });
 });
