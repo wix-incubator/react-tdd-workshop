4e1ef7e92ed8074c515b15392699a4018a5d5582
Yaniv Efraim <yaniv.efraim@gmail.com>
2017-10-20 17:38:45 +0300

    refactor: move state to top

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,12 +5,28 @@ import s from './App.scss';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {winner: ''};
+    this.state = {
+      winner: '',
+      board: [...Array(3)].map(() => Array(3).fill('')),
+      nextPlayer: 'X'
+    };
   }
+
+  onGameChanged = (rowIndex, cellIndex) => {
+    const nextPlayer = this.state.nextPlayer;
+    const board = [...this.state.board];
+    board[rowIndex][cellIndex] = nextPlayer;
+    if (board[0].every(cell => cell === nextPlayer)) {
+      this.setState({winner: nextPlayer});
+    }
+    const newNextPlayer = this.state.nextPlayer === 'X' ? 'O' : 'X';
+    this.setState({board, nextPlayer: newNextPlayer});
+  }
+
   render() {
     return (
       <div data-hook="app" className={s.root}>
-        <Board onGameOver={winner => this.setState({winner})}/>
+        <Board handleGameChanged={this.onGameChanged} board={this.state.board}/>
         {this.state.winner && <div data-hook="winner-message">{`${this.state.winner} Wins!`}</div>}
       </div>
     );
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,44 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Board extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      board: [...Array(3)].map(() => Array(3).fill('')),
-      nextPlayer: 'X'
-    };
-  }
-
-  cellClicked(rowIndex, cellIndex) {
-    const board = [...this.state.board];
-    board[rowIndex][cellIndex] = this.state.nextPlayer;
-    if (board[0].every(cell => cell === this.state.nextPlayer)) {
-      this.props.onGameOver(this.state.nextPlayer);
-    }
-    const nextPlayer = this.state.nextPlayer === 'X' ? 'O' : 'X';
-    this.setState({board, nextPlayer});
-  }
-
-  render() {
-    return (<table>
-      <tbody>
-        {this.state.board.map((row, rowIndex) =>
-          <tr key={rowIndex}>{row.map((cell, cellIndex) =>
-            <td
-              data-hook="cell"
-              key={cellIndex}
-              onClick={() => this.cellClicked(rowIndex, cellIndex)}
-              >{cell}</td>)}
-          </tr>
-        )}
-      </tbody>
-    </table>);
-  }
-}
+const Board = ({board, handleGameChanged}) => (<table>
+  <tbody>
+    {board.map((row, rowIndex) =>
+      <tr key={rowIndex}>{row.map((cell, cellIndex) =>
+        <td
+          data-hook="cell"
+          key={cellIndex}
+          onClick={() => handleGameChanged(rowIndex, cellIndex)}
+          >{cell}</td>)}
+      </tr>
+    )}
+  </tbody>
+</table>);
 
 Board.propTypes = {
-  onGameOver: PropTypes.func.isRequired
+  board: PropTypes.array.isRequired,
+  handleGameChanged: PropTypes.func.isRequired
 };
 
 export default Board;
